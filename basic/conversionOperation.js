7b2944b9d46a3020c5dc78894ef48281c9756e17
Jitendra834 <jituprajapati834@gmail.com>
2025-08-10 09:45:01 +0000

    conversion of datatype

---
+++ basic/conversionOperation.js
@@ -0,0 +1,18 @@
+let a = "54"
+let b = "123abc"
+
+console.log(a);
+console.log(typeof a);          // string
+
+let valueInNumber1 = Number(a)
+let valueInNumber2 = Number(b)
+
+console.log(typeof valueInNumber);  // number
+console.log(valueInNumber1)         // 54
+console.log(valueInNumber2)         //NaN
+
+
+// "54" convert to => 54
+// "54abc" convert to => NaN
+// true convert to => 1; false => 0
+ 
