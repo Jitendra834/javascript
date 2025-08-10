cac2bd0f74bc0c014c9b80df36e77d10db55faa0
Jitendra834 <jituprajapati834@gmail.com>
2025-08-10 09:46:02 +0000

    done conversion of datatype

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -15,4 +15,10 @@ console.log(valueInNumber2)         //NaN
 // "54" convert to => 54
 // "54abc" convert to => NaN
 // true convert to => 1; false => 0
- 
+
+
+// Convert number to string
+let someNumber = 123
+let someString = String(someNumber)
+console.log(someString);            // "123"
+console.log(typeof someString);     // string
