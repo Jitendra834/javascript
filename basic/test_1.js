b233365602133ea5e1e78e0590ab4be75f9c0863
Jitendra834 <jituprajapati834@gmail.com>
2025-08-10 09:01:42 +0000

    done

--- basic/test_1.js
+++ basic/test_1.js
@@ -1,1 +1,7 @@
 console.log("Hello")
+const accountId = 50100085464609
+let accountEmail = "jitu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmeedabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
