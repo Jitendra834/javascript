dfa8f05836319d77b520341d260d458c34a2bfc9
Jitendra834 <jituprajapati834@gmail.com>
2025-08-13 18:31:34 +0000

    String save 1

---
+++ basic/05_strings.js
@@ -0,0 +1,13 @@
+const name = "Jitendrakumar Prajapati"
+const age = 21
+console.log( name + age + " Values")
+console.log(`Hello my name is ${name} and I am ${age} years old.`)
+
+const Name = new String("JitendraKumara Gamanbhai Prajapati") // use when need more functions Property
+console.log(Name.toUpperCase())   // change to UpperCase
+console.log(Name.charAt(5))       //find Index to character
+console.log(Name.indexOf('P'))          // find Index of character
+console.log(Name.substring(0, 8)) // find substring
+console.log(Name.replace("Kumara", "Bhai"))     // change 
+console.log(Name.split(" "))    // split to array
+// video = 2.29.18
