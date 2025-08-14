8ccfafaa4306259d489e43c536022736cdb3d65e
Jitendra834 <jituprajapati834@gmail.com>
2025-08-14 18:14:05 +0000

    01_arrays_2

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,11 @@
+const myarray = [1, 2, 3, 4, 5];
+console.log(myarray.length); // Output: 5
+console.log(myarray[0]);     // Output: 1
+console.log(myarray);  // Output: [1, 2, 3, 4, 5]
+
+
+
+const myHeros = ["Spider-Man", "Iron Man", "Captain America"];
+console.log(myHeros.length); // Output: 3
+console.log(myHeros[0]);     // Output: Spider-Man
+console.log(myHeros);        // Output: ["Spider-Man", "Iron Man", "Captain America"]
